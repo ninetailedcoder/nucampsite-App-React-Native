9b88a0ff288eec09fa6ef2e5fd3be12520969969
Tory DeMaio <torydemaio@gmail.com>
2023-03-27 21:53:14 -0500

    Icons, Favorites and Comments

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,9 +1,61 @@
 import RenderCampsite from '../features/campsites/RenderCampsite'
-
+import { useState } from 'react'
+import { FlatList, StyleSheet,Text, View  } from 'react-native'
+import {COMMENTS} from '../shared/comments'
 
 const CampsiteInfoScreen = ({route}) => {
     const {campsite} = route.params
-    return <RenderCampsite campsite={campsite} />
+    const [comments, setComments] =useState(COMMENTS)
+    const [favorite, setFavorite] = useState(false)
+
+    const renderCommentItem=({item})=>{
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating}</Text>
+                <Text style={{fontSize: 12}}>{`--${item.author}, ${item.date}`}</Text>
+            </View>
+        )
+    }
+    return (
+        <FlatList
+            data={comments.filter(
+                (comment)=> comment.campsiteId === campsite.id
+            )}
+            renderItem={renderCommentItem}
+            keyExtractor={(item)=> item.id.toString()}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite 
+                        isFavorite={favorite}
+                        markFavorite ={()=> setFavorite(true)}
+                        campsite={campsite} />
+                    <Text style={styles.commentsTitle}>Comments</Text>
+                </>
+            }
+        />
+    )
+    
 }
+const styles = StyleSheet.create({
+    commentsTitle:{
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#43484d',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    }
+})
 
 export default CampsiteInfoScreen
